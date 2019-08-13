48d8a86663028ddccd63ddd20b71dd96cb971cca
Chris Sainty <chrissainty@users.noreply.github.com>
2019-08-13 23:49:08 +0100

    Upgrade to p8

---
+++ src/Blazored.Localisation/Interop/blazored-localisation.js
@@ -0,0 +1,48 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var Blazored;
+(function (Blazored) {
+    var Localisation;
+    (function (Localisation_1) {
+        var Localisation = /** @class */ (function () {
+            function Localisation() {
+            }
+            Localisation.prototype.GetBrowserLocale = function () {
+                return (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator['userLanguage']
+                    || navigator.language
+                    || navigator['browserLanguage']
+                    || 'en';
+            };
+            Localisation.prototype.GetBrowserTimeZoneOffset = function () {
+                return new Date().getTimezoneOffset();
+            };
+            Localisation.prototype.GetBrowserTimeZoneIdentifier = function () {
+                return Intl.DateTimeFormat().resolvedOptions().timeZone;
+            };
+            return Localisation;
+        }());
+        function Load() {
+            var localisation = {
+                Localisation: new Localisation()
+            };
+            if (window['Blazored']) {
+                window['Blazored'] = __assign({}, window['Blazored'], localisation);
+            }
+            else {
+                window['Blazored'] = __assign({}, localisation);
+            }
+        }
+        Localisation_1.Load = Load;
+    })(Localisation = Blazored.Localisation || (Blazored.Localisation = {}));
+})(Blazored || (Blazored = {}));
+Blazored.Localisation.Load();
+//# sourceMappingURL=blazored-localisation.js.map
--- src/Blazored.Localisation/wwwroot/blazored-localisation.js
+++ src/Blazored.Localisation/wwwroot/blazored-localisation.js
@@ -1,44 +1,30 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 var Blazored;
 (function (Blazored) {
     var Localisation;
     (function (Localisation_1) {
-        var Localisation = /** @class */ (function () {
-            function Localisation() {
-            }
-            Localisation.prototype.GetBrowserLocale = function () {
+        class Localisation {
+            GetBrowserLocale() {
                 return (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator['userLanguage']
                     || navigator.language
                     || navigator['browserLanguage']
                     || 'en';
-            };
-            Localisation.prototype.GetBrowserTimeZoneOffset = function () {
+            }
+            GetBrowserTimeZoneOffset() {
                 return new Date().getTimezoneOffset();
-            };
-            Localisation.prototype.GetBrowserTimeZoneIdentifier = function () {
+            }
+            GetBrowserTimeZoneIdentifier() {
                 return Intl.DateTimeFormat().resolvedOptions().timeZone;
-            };
-            return Localisation;
-        }());
+            }
+        }
         function Load() {
-            var localisation = {
+            const localisation = {
                 Localisation: new Localisation()
             };
             if (window['Blazored']) {
-                window['Blazored'] = __assign({}, window['Blazored'], localisation);
+                window['Blazored'] = Object.assign({}, window['Blazored'], localisation);
             }
             else {
-                window['Blazored'] = __assign({}, localisation);
+                window['Blazored'] = Object.assign({}, localisation);
             }
         }
         Localisation_1.Load = Load;
